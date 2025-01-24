6a64e2ab85a46a73809e9440fc01ef23048dcea7
Majda-Fg <majda.fougou@gmail.com>
2025-01-24 12:26:24 +0100

    étape 1.2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// étape 1.1 - une ligne d'étoiles
+// étape 1.1 - une ligne d'étoiles //sans console.log('*')
 
 function afficherEtoiles(n){
   let singleLine = '';
@@ -6,11 +6,22 @@ function afficherEtoiles(n){
     singleLine += '*'
   }
   console.log(singleLine)
-    }
-    afficherEtoiles(2)
-    afficherEtoiles(9)
+}
+afficherEtoiles(2)
+afficherEtoiles(9)
 
-    
+// étape 1.2 - Carré d'étoiles // sans appel de fonction
+function afficherRectangle(hauteur,largeur){
+    for (let i = 1; i <= hauteur; i++) {
+    let ligne = ""; 
+        for (let j = 1; j <= largeur; j++) {
+         ligne += '*';
+        }
+    console.log(ligne); 
+    }
+}
+afficherRectangle(5,5)
+
 // let singleLine = "";
 // for (let i = 0; i< n; i++){
    
