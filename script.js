5bbfff1cbe75bd16dbdefe1dfa871ba4a8d2edaf
Majda-Fg <majda.fougou@gmail.com>
2025-01-24 13:05:12 +0100

    étape 1.3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // étape 1.1 - une ligne d'étoiles //sans console.log('*')
-
 function afficherEtoiles(n){
+    console.log("étape 1.1")
   let singleLine = '';
   for(let i=0; i<n; i++){
     singleLine += '*'
@@ -12,6 +12,7 @@ afficherEtoiles(9)
 
 // étape 1.2 - Carré d'étoiles // sans appel de fonction
 function afficherRectangle(hauteur,largeur){
+    console.log("étape 1.2")
     for (let i = 1; i <= hauteur; i++) {
     let ligne = ""; 
         for (let j = 1; j <= largeur; j++) {
@@ -22,32 +23,33 @@ function afficherRectangle(hauteur,largeur){
 }
 afficherRectangle(5,5)
 
+// étape 1.3 - Triangle d'étoiles 
+function triangleDroite(n){
+    console.log("étape 1.3")
+    let i = 0;
+    let singleLine = ''
+    do{ singleLine += '*';
+        i++;
+        console.log(singleLine+"\\")
+    }while(i < n)
+    }
+triangleDroite(5)
+
+
 // let singleLine = "";
 // for (let i = 0; i< n; i++){
    
 //     console.log('*')
 // }
 
-
 // let str = '',i=1;
 // while(i<=10){
 //     str += i+'';
 //     i += 1;
 // }
-
 // console.log(str);
 
 // function afficherPointeSapin(hauteur) {
 //     // à remplir
 //   }
 //   afficherPointeSapin(4)
-
-// function triangleDroite(n){
-//     let i = 0;
-//     let singleLine = ''
-//     do{ singleLine += '*'+''
-//         i++;
-//         console.log(singleLine)
-//     }while( i < n)
-//     }
-//     afficherEtoiles(5)
