d6498a2784d0a3e4bc03934d47edabf8ec26d293
Majda-Fg <majda.fougou@gmail.com>
2025-01-24 10:46:36 +0100

    étape 1.1

---
+++ script.js
@@ -0,0 +1,42 @@
+// étape 1.1 - une ligne d'étoiles
+
+function afficherEtoiles(n){
+  let singleLine = '';
+  for(let i=0; i<n; i++){
+    singleLine += '*'
+  }
+  console.log(singleLine)
+    }
+    afficherEtoiles(2)
+    afficherEtoiles(9)
+
+    
+// let singleLine = "";
+// for (let i = 0; i< n; i++){
+   
+//     console.log('*')
+// }
+
+
+// let str = '',i=1;
+// while(i<=10){
+//     str += i+'';
+//     i += 1;
+// }
+
+// console.log(str);
+
+// function afficherPointeSapin(hauteur) {
+//     // à remplir
+//   }
+//   afficherPointeSapin(4)
+
+// function triangleDroite(n){
+//     let i = 0;
+//     let singleLine = ''
+//     do{ singleLine += '*'+''
+//         i++;
+//         console.log(singleLine)
+//     }while( i < n)
+//     }
+//     afficherEtoiles(5)
